8fde3d3d8b79074a751f63405fb3783a8aae0a52
Eric Martinez Acelas <ise.projects@outlook.com>
2020-07-29 21:59:41 -0500

    90%

---
+++ js/chart.js
@@ -0,0 +1,35 @@
+//radar
+var ctxR = document.getElementById("radarChart").getContext('2d');
+var myRadarChart = new Chart(ctxR, {
+    type: 'radar',
+    data: {
+        labels: ["Filosofía del riesgo", "Manejo de herramientas", "Comunicación y relación",
+         "Auto aprendizaje", "capacidad resolutiva", "Trabajo en equipo", "Objetividad"],
+        datasets: [{
+                label: "Developer",
+                data: [75, 73, 73, 75, 77, 75, 67],
+                backgroundColor: [
+                    'rgba(05, 10, 202, .2)',
+                ],
+                borderColor: [
+                    'rgb(319, 136, 153)',
+                ],
+                borderWidth: 2
+            },
+            {
+                label: "ST Climatización",
+                data: [80, 75, 75, 77, 80, 77, 80],
+                backgroundColor: [
+                    'rgba(105, 10, 02, .2)',
+                ],
+                borderColor: [
+                    'rgb(119, 136, 153)',
+                ],
+                borderWidth: 2
+            }
+        ]
+    },
+    options: {
+        responsive: true
+    }
+});
